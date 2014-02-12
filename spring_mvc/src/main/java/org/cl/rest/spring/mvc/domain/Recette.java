621a4c31c4ee699c491fd02771202602e1ad721a
cl <cl>
2014-02-12 13:55:03 +0100

    mise en place swagger sur partie spring/jersey 2

--- spring_mvc/src/main/java/org/cl/rest/spring/mvc/domain/Recette.java
+++ spring_mvc/src/main/java/org/cl/rest/spring/mvc/domain/Recette.java
@@ -2,19 +2,15 @@ package org.cl.rest.spring.mvc.domain;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-<<<<<<< HEAD
 import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
 
-=======
->>>>>>> inclusion de domain et dao : spring:mvc
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @XmlRootElement
 @EqualsAndHashCode(of = { "libelle" })
-<<<<<<< HEAD
 @ApiModel(value = "Une recette de desserts.", description = "Une recette de dessert, avec sa description, le niveau de dificulté et le temps de realisation nécessaire")
 public class Recette {
 
@@ -27,14 +23,6 @@ public class Recette {
 	@ApiModelProperty(value = "Le temps nécessaire à la réalisation de la recette en minutes", required=false)
 	private int temps;
 	@ApiModelProperty(value = "Les étapes necessaires à la réalisation de la recette", required=false)
-=======
-public class Recette {
-
-	private String id;
-	private String libelle;
-	private int niveau;
-	private int temps;
->>>>>>> inclusion de domain et dao : spring:mvc
 	private String recette;
 
 }
